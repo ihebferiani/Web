047defc2faf74f617249d8bd498f4c78b21722b7
Iheb Feriani <42569011+ihebferiani@users.noreply.github.com>
2019-11-29 01:35:11 +0100

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,74 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>cart</title>
+		<link rel="stylesheet" href="views/css/bootstrap.min.css"/>
+		<script src="views/js/jquery2.js"></script>
+		<script src="views/js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+	</head>
+<body>
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" id="cart_msg">
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Panier</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-2 col-xs-2"><b>Action</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Image</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Name</b></div>
+							<div class="col-md-2 col-xs-2"><b>Quantity</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Price</b></div>
+							<div class="col-md-2 col-xs-2"><b>Total</b></div>
+
+						</div>
+						<div id="cart_checkout"></div>
+						
+						</div> 
+					</div>
+					<div class="panel-footer"></div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+		</div>
+</body>	
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
---
+++ index.php
@@ -0,0 +1,144 @@
+<?php
+session_start();
+if(isset($_SESSION["uid"])){
+	header("location:profile.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Trésors De Terroirs</title>
+		<link rel="stylesheet" href="views/css/bootstrap.min.css"/>
+		<script src="views/js/jquery2.js"></script>
+		<script src="views/js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<style></style>
+	</head>
+<body>
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+					<span class="sr-only">navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+
+		
+			<ul class="nav navbar-nav navbar-right">
+			<li><a href="login.php" class="wish"></span>Log In <span class="login"></span></a>
+
+				<li><a href="wishlist.php" class="wish"></span>Wishlist <span class="badgew">0</span></a>
+
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-shopping-cart"></span>Cart<span class="badge">0</span></a>
+					<div class="dropdown-menu" style="width:400px;">
+						<div class="panel panel-success">
+							<div class="panel-heading">
+								<div class="row">
+								<div class="col-md-3">Product ID</div>
+
+									<div class="col-md-3">Product Image</div>
+									<div class="col-md-3">Product Name</div>
+									<div class="col-md-3">Price</div>
+								</div>
+							</div>
+							<div class="panel-body">
+								<div id="cart_product">
+							
+								</div>
+							</div>
+							<div class="panel-footer"></div>
+						</div>
+					</div>
+				</li>
+				
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>	
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-1"></div>
+
+			<div class="col-md-8 col-xs-12">
+				<div class="row">
+					<div class="col-md-12 col-xs-12" id="product_msg">
+					<div class="col-md-12 col-xs-12" id="product_msgg">
+
+					</div>
+				</div>
+				<div class="panel panel-info">
+					<div class="panel-heading">Products</div>
+					<div class="panel-body">
+						<div id="get_product">
+						</div>
+			
+					</div>
+					<div class="panel-footer"></div>
+				</div>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
